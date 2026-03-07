b8be1cdafa2f90b8822f8ca580648ebf1d1b2ee4
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2026-03-07 13:34:23 +0200

    added edit to user ui(where you can choose prorities and date), and also added TaskItem class

--- TodoLearn/MainPage.xaml.cs
+++ TodoLearn/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 using TodoLearn.Models;
 
 namespace TodoLearn
@@ -6,6 +7,8 @@ namespace TodoLearn
     public partial class MainPage : ContentPage
     {
         public ObservableCollection<TaskItem> Tasks { get; } = new ObservableCollection<TaskItem>();
+        public ObservableCollection<TaskItem> DisplayTasks { get; } = new ObservableCollection<TaskItem>();
+        private string _currentFilter = "All";
 
         public MainPage()
         {
@@ -14,7 +17,12 @@ namespace TodoLearn
 
             Tasks.Add(new TaskItem { Text = "Finish project report", Priority = TaskPriority.High, DueAt = DateTime.Now.AddHours(4) });
             Tasks.Add(new TaskItem { Text = "Buy groceries", Priority = TaskPriority.Low, DueAt = DateTime.Now.AddDays(1) });
+
+            Tasks.CollectionChanged += (_, __) => RefreshDisplay();
+            RefreshDisplay();
         }
+
+        
 
         private void OnAddTaskClicked(object? sender, EventArgs e)
         {
@@ -22,6 +30,33 @@ namespace TodoLearn
             if (string.IsNullOrWhiteSpace(text)) return;
             Tasks.Add(new TaskItem { Text = text });
             NewTaskEntry.Text = string.Empty;
+        }
+
+        private void OnFilterClicked(object? sender, EventArgs e) { }
+
+        private void RefreshDisplay()
+        {
+            DisplayTasks.Clear();
+            IEnumerable<TaskItem> items = Tasks;
+            switch (_currentFilter)
+            {
+                case "Important":
+                    items = Tasks.Where(t => t.Priority == TaskPriority.High);
+                    break;
+                case "Planned":
+                    items = Tasks.Where(t => t.DueAt > DateTime.Now);
+                    break;
+                case "All":
+                    items = Tasks;
+                    break;
+                case "MyDay":
+                default:
+                    items = Tasks.Where(t => t.DueAt.Date == DateTime.Now.Date);
+                    break;
+            }
+
+            foreach (var it in items)
+                DisplayTasks.Add(it);
         }
 
         private void OnClearCompletedClicked(object? sender, EventArgs e)
@@ -44,7 +79,7 @@ namespace TodoLearn
         private void OnPriorityPointerEntered(object? sender, EventArgs e)
         {
             if (sender is BindableObject b && b.BindingContext is TaskItem t)
-            {
+            { 
                 t.IsDetailsVisible = true;
             }
         }
@@ -54,6 +89,56 @@ namespace TodoLearn
             if (sender is BindableObject b && b.BindingContext is TaskItem t)
             {
                 t.IsDetailsVisible = false;
+            }
+        }
+
+        private async void OnPriorityTapped(object? sender, EventArgs e)
+        {
+            if (sender is BindableObject b && b.BindingContext is TaskItem t)
+            {
+                t.IsDetailsVisible = !t.IsDetailsVisible;
+            }
+        }
+
+        
+
+        private void OnEditClicked(object? sender, EventArgs e)
+        {
+            if (sender is Button btn && btn.BindingContext is TaskItem t)
+            {
+                t.IsEditing = true;
+            }
+        }
+
+        private void OnSaveClicked(object? sender, EventArgs e)
+        {
+            if (sender is Button btn && btn.BindingContext is TaskItem t)
+            {
+                t.IsEditing = false;
+            }
+        }
+
+        private void OnCancelClicked(object? sender, EventArgs e)
+        {
+            if (sender is Button btn && btn.BindingContext is TaskItem t)
+            {
+                t.IsEditing = false;
+                // пофиксить с канселом - чтобы сохраняять
+            }
+        }
+
+        private void OnPrioritySelectClicked(object? sender, EventArgs e)
+        {
+            if (sender is Button btn && btn.CommandParameter is string s && btn.BindingContext is TaskItem t)
+            {
+                if (int.TryParse(s, out var idx))
+                {
+                    t.Priority = (TaskPriority)idx;
+                }
+            }
+            else if (sender is Button b && b.CommandParameter is int i && b.BindingContext is TaskItem ti)
+            {
+                ti.Priority = (TaskPriority)i;
             }
         }
     }
--- TodoLearn/Models/TaskItem.cs
+++ TodoLearn/Models/TaskItem.cs
@@ -19,11 +19,69 @@ namespace TodoLearn.Models
             Priority = TaskPriority.Low;
         }
 
-        public string? Text { get; set; }
+        private string? _text;
+        public string? Text
+        {
+            get => _text;
+            set
+            {
+                if (_text == value) return;
+                _text = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+            }
+        }
 
         public DateTime CreatedAt { get; }
 
-        public DateTime DueAt { get; set; }
+        private DateTime _dueAt;
+        public DateTime DueAt
+        {
+            get => _dueAt;
+            set
+            {
+                if (_dueAt == value) return;
+                _dueAt = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DueAt)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DueDate)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DueTime)));
+            }
+        }
+
+        private bool _isEditing;
+        public bool IsEditing
+        {
+            get => _isEditing;
+            set
+            {
+                if (_isEditing == value) return;
+                _isEditing = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsEditing)));
+            }
+        }
+
+        public DateTime DueDate
+        {
+            get => DueAt.Date;
+            set => DueAt = value.Date + DueAt.TimeOfDay;
+        }
+
+        public TimeSpan DueTime
+        {
+            get => DueAt.TimeOfDay;
+            set => DueAt = DueAt.Date + value;
+        }
+
+        private bool _isCompleted;
+        public bool IsCompleted
+        {
+            get => _isCompleted;
+            set
+            {
+                if (_isCompleted == value) return;
+                _isCompleted = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsCompleted)));
+            }
+        }
 
         private TaskPriority _priority;
         public TaskPriority Priority
@@ -34,7 +92,14 @@ namespace TodoLearn.Models
                 if (_priority == value) return;
                 _priority = value;
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Priority)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PriorityIndex)));
             }
+        }
+
+        public int PriorityIndex
+        {
+            get => (int)Priority;
+            set => Priority = (TaskPriority)value;
         }
 
         private bool _isDetailsVisible;
